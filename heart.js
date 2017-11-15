ca7f767bfadcd9131b351c318914b78829bb7291
Ines <ines.opcoelho@gmail.com>
2017-11-15 01:40:04 +0000

    create heart

---
+++ heart.js
@@ -0,0 +1,17 @@
+$('.sponsorsBox').magnificPopup({
+	type: 'image',
+	closeBtnInside: false,
+	mainClass: 'mfp-with-zoom mfp-img-mobile',
+
+	image: {
+		verticalFit: true,
+		titleSrc: function(item) {
+    return item.el.attr('title');
+		}
+	},
+	gallery: {
+	  enabled: true,
+	  navigateByImgClick: true,
+	  preload: [0, 1]
+	}, 
+});
